63869a7490a489d2b548a1bfec1c1b7bc345f699
qasimgit <qasimpkkhi@gmail.com>
2020-09-13 18:06:37 +0500

    after changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
    <div className="container">
-     <h1 className="heading">Github Actions Demo</h1>
+     <h1 className="heading">Github Actions Demo.</h1>
    </div>
   );
 }
